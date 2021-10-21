dc96ba277e7dc1e4e4f1ab2747cfba49892603a0
tdevere <tdevere@microsoft.com>
2021-10-21 17:06:43 -0500

    updated script to print Xamarin.Android.CSharp.targets

--- TargetVersion/appcenter-pre-build.sh
+++ TargetVersion/appcenter-pre-build.sh
@@ -1,3 +1,8 @@
 ﻿#!/usr/bin/env bash
-
+echo "Print project.assets.json"
 find /$APPCENTER_SOURCE_DIRECTORY -type f -name project.assets.json -exec cat -n {} +
+
+Xamarin.Android.Common.targets
+
+echo "Print Xamarin.Android.CSharp.targets"
+find /$(MSBuildExtensionsPath) -type f -name Xamarin.Android.CSharp.targets -exec cat -n {} +
