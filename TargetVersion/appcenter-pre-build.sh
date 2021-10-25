7ac20e9cb029e9379902dc05563a98ff3ccc8986
tdevere <tdevere@microsoft.com>
2021-10-25 13:56:00 -0500

    Enable Verbose Build within Build Script

--- TargetVersion/appcenter-pre-build.sh
+++ TargetVersion/appcenter-pre-build.sh
@@ -1,9 +1,20 @@
 ﻿#!/usr/bin/env bash
-echo "project item tony"
-echo "Print project.assets.json"
-find /$APPCENTER_SOURCE_DIRECTORY -type f -name project.assets.json -exec cat -n {} +
-
-Xamarin.Android.Common.targets
-
-echo "Print Xamarin.Android.CSharp.targets"
+echo "Project Level Build Script"
+#echo "Print project.assets.json"
+#find /$APPCENTER_SOURCE_DIRECTORY -type f -name project.assets.json -exec cat -n {} +
+#Xamarin.Android.Common.targets
+#echo "Print Xamarin.Android.CSharp.targets"
 #find /$(MSBuildExtensionsPath) -type f -name Xamarin.Android.CSharp.targets -exec cat -n {} +
+
+#echo "Git Create Branch"
+#uploadBranch=AppCenterSupport
+#finalBranchName="${$APPCENTER_BUILD_ID}_${uploadBranch}"
+#git branch $finalBranchName
+
+#echo "List branches"
+#git branch
+
+echo "Start Verbose Build"
+/Library/Frameworks/Mono.framework/Versions/6_12_7/bin/msbuild /version /nologo
+/Library/Frameworks/Mono.framework/Versions/6_12_7/bin/msbuild /Users/runner/work/1/s/TargetVersion/TargetVersion.csproj /t:PackageForAndroid -t:SignAndroidPackage -p:AndroidPackageFormat=aab /p:Configuration=Release /Library/Frameworks/Mono.framework/Versions/6_12_7/bin/msbuild /Users/runner/work/1/s/TargetVersion/TargetVersion.csproj /t:PackageForAndroid -t:SignAndroidPackage -p:AndroidPackageFormat=aab /p:Configuration=Release -v:diag
+
