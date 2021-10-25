e450ba442b807cc076e162d853ea0bbc6c1ff6f4
tdevere <tdevere@microsoft.com>
2021-10-25 14:30:46 -0500

    fixed list folders

--- TargetVersion/appcenter-pre-build.sh
+++ TargetVersion/appcenter-pre-build.sh
@@ -14,7 +14,7 @@ find /$APPCENTER_SOURCE_DIRECTORY -type f -name project.assets.json -exec cat -n
 #echo "List branches"
 #git branch
 echo "List Frameworks"
-ls /Library/Frameworks/Mono.framework/Versions/
+ls -d /Library/Frameworks/Mono.framework/Versions/*
 
 echo "Start Verbose Build"
 /Library/Frameworks/Mono.framework/Versions/6_12_7/bin/msbuild /version /nologo
