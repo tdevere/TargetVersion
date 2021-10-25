e28bcb639099bde8a7023b62120c7c93ba0b7526
tdevere <tdevere@microsoft.com>
2021-10-25 14:27:52 -0500

    restored build version and added list

--- TargetVersion/appcenter-pre-build.sh
+++ TargetVersion/appcenter-pre-build.sh
@@ -13,8 +13,10 @@ find /$APPCENTER_SOURCE_DIRECTORY -type f -name project.assets.json -exec cat -n
 
 #echo "List branches"
 #git branch
+echo "List Frameworks"
+ls /Library/Frameworks/Mono.framework/Versions/
 
 echo "Start Verbose Build"
-/Library/Frameworks/Mono.framework/Versions/6_4_0/bin/msbuild /version /nologo
-/Library/Frameworks/Mono.framework/Versions/6_4_0/bin/msbuild /Users/runner/work/1/s/TargetVersion/TargetVersion.csproj /t:PackageForAndroid -t:SignAndroidPackage -p:AndroidPackageFormat=aab /p:Configuration=Release -v:diag
+/Library/Frameworks/Mono.framework/Versions/6_12_7/bin/msbuild /version /nologo
+/Library/Frameworks/Mono.framework/Versions/6_12_7/bin/msbuild /Users/runner/work/1/s/TargetVersion/TargetVersion.csproj /t:PackageForAndroid -t:SignAndroidPackage -p:AndroidPackageFormat=aab /p:Configuration=Release -v:diag
 
