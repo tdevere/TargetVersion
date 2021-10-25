b4266f0252db6dc5e9b4152e03ab52a197897905
tdevere <tdevere@microsoft.com>
2021-10-25 14:43:15 -0500

    removed extra build

--- TargetVersion/appcenter-pre-build.sh
+++ TargetVersion/appcenter-pre-build.sh
@@ -1,7 +1,7 @@
 ﻿#!/usr/bin/env bash
 echo "Project Level Build Script"
-echo "Print project.assets.json"
-find /$APPCENTER_SOURCE_DIRECTORY -type f -name project.assets.json -exec cat -n {} +
+#echo "Print project.assets.json"
+#find /$APPCENTER_SOURCE_DIRECTORY -type f -name project.assets.json -exec cat -n {} +
 #Xamarin.Android.Common.targets
 #echo "Print Xamarin.Android.CSharp.targets"
 #find /$(MSBuildExtensionsPath) -type f -name Xamarin.Android.CSharp.targets -exec cat -n {} +
@@ -13,10 +13,10 @@ find /$APPCENTER_SOURCE_DIRECTORY -type f -name project.assets.json -exec cat -n
 
 #echo "List branches"
 #git branch
-echo "List Frameworks"
-ls -d /Library/Frameworks/Mono.framework/Versions/*
+#echo "List Frameworks"
+#ls -d /Library/Frameworks/Mono.framework/Versions/*
 
 echo "Start Verbose Build"
-/Library/Frameworks/Mono.framework/Versions/6_12_7/bin/msbuild /version /nologo
-/Library/Frameworks/Mono.framework/Versions/6_12_7/bin/msbuild /Users/runner/work/1/s/TargetVersion/TargetVersion.csproj /t:PackageForAndroid -t:SignAndroidPackage -p:AndroidPackageFormat=aab /p:Configuration=Release -v:diag
+#/Library/Frameworks/Mono.framework/Versions/6_12_7/bin/msbuild /version /nologo
+#/Library/Frameworks/Mono.framework/Versions/6_12_7/bin/msbuild /Users/runner/work/1/s/TargetVersion/TargetVersion.csproj /t:PackageForAndroid -t:SignAndroidPackage -p:AndroidPackageFormat=aab /p:Configuration=Release -v:diag
 
