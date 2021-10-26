f32b3b6b93668eda3966cce4d4f6418000079cd4
tdevere <tdevere@microsoft.com>
2021-10-25 19:37:58 -0500

    appcenter --version

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -3,5 +3,6 @@
 #find /$APPCENTER_SOURCE_DIRECTORY -type f -name project.assets.json -exec cat -n {} +
 
 echo "upgrading npm i appcenter-cli"
+appcenter --version
 npm i appcenter-cli@latest
 appcenter --version
