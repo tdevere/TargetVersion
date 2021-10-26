ec62083f3fa3f47e051eb5b61ae748960529fc91
tdevere <tdevere@microsoft.com>
2021-10-25 19:45:39 -0500

    moved to new script

--- TargetVersion/appcenter-pre-build.sh
+++ TargetVersion/appcenter-pre-build.sh
@@ -1,5 +1,9 @@
 ﻿#!/usr/bin/env bash
 echo "Project Level Build Script"
+echo "upgrading npm i appcenter-cli"
+appcenter --version
+npm i appcenter-cli@latest
+appcenter --version
 #echo "Print project.assets.json"
 #find /$APPCENTER_SOURCE_DIRECTORY -type f -name project.assets.json -exec cat -n {} +
 #Xamarin.Android.Common.targets
@@ -16,13 +20,13 @@ echo "Project Level Build Script"
 #echo "List Frameworks"
 #ls -d /Library/Frameworks/Mono.framework/Versions/*
 
-echo "cat $AGENT_HOMEDIRECTORY/scripts/select-xamarin-sdk.sh"
-cat $AGENT_HOMEDIRECTORY/scripts/select-xamarin-sdk.sh
+#echo "cat $AGENT_HOMEDIRECTORY/scripts/select-xamarin-sdk.sh"
+#cat $AGENT_HOMEDIRECTORY/scripts/select-xamarin-sdk.sh
 
-echo "Use Older version of Mono"
-/bin/bash $AGENT_HOMEDIRECTORY/scripts/select-xamarin-sdk.sh 6_4_0
+#echo "Use Older version of Mono"
+#/bin/bash $AGENT_HOMEDIRECTORY/scripts/select-xamarin-sdk.sh 6_4_0
 
-echo "Start Verbose Build"
-/Library/Frameworks/Mono.framework/Versions/6_4_0/bin/msbuild /version /nologo
-/Library/Frameworks/Mono.framework/Versions/6_4_0/bin/msbuild /Users/runner/work/1/s/TargetVersion/TargetVersion.csproj -t:SignAndroidPackage -p:AndroidPackageFormat=aab /p:Configuration=Release -v:diag
+#echo "Start Verbose Build"
+#/Library/Frameworks/Mono.framework/Versions/6_4_0/bin/msbuild /version /nologo
+#/Library/Frameworks/Mono.framework/Versions/6_4_0/bin/msbuild /Users/runner/work/1/s/TargetVersion/TargetVersion.csproj -t:SignAndroidPackage -p:AndroidPackageFormat=aab /p:Configuration=Release -v:diag
 
